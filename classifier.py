ee478521afb4b504035274b468b04d7ae76848bd
Richard Halbert <rvh2509@outlook.com>
2022-03-14 23:53:09 -0800

    Updated classifier.py

--- classifier.py
+++ classifier.py
@@ -1,6 +1,5 @@
 import torch
 from torch.utils.data import DataLoader
-import torchvision
 import torchvision.transforms as transforms
 from torchvision.datasets import ImageFolder
 import matplotlib.pyplot as plt
@@ -14,7 +13,7 @@ TEST_PATH = './train'
 
 transform = transforms.Compose([
     transforms.ToTensor(), # make them tensors
-    transforms.Resize((64, 64)) # make all images the same size
+    transforms.Resize((256, 256)), # make all images the same size
 ])
 
 trainset = ImageFolder(TRAIN_PATH, transform=transform)
@@ -26,47 +25,47 @@ testloader = DataLoader(testset, batch_size=128, shuffle=False, num_workers=2)
 classes = trainset.classes
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
-class Darknet64(nn.Module):
-    def __init__(self):
-        super(Darknet64, self).__init__() # https://pytorch.org/docs/stable/generated/torch.nn.Conv2d.html
-        self.conv1 = nn.Conv2d(3, 16, 3, padding=1, bias=False)
-        self.bn1 = nn.BatchNorm2d(16)
+class DarknetUpdated(nn.Module):
+  def __init__(self):
+    super(DarknetUpdated, self).__init__()
+    self.conv1 = nn.Conv2d(3, 16, 3, padding=1, bias=False)
+    self.bn1 = nn.BatchNorm2d(16)
 
-        self.conv2 = nn.Conv2d(16, 32, 3, padding=1, bias=False)
-        self.bn2 = nn.BatchNorm2d(32)
+    self.conv2 = nn.Conv2d(16, 32, 3, padding=1, bias=False)
+    self.bn2 = nn.BatchNorm2d(32)
 
-        self.conv3 = nn.Conv2d(32, 64, 3, padding=1, bias=False)
-        self.bn3 = nn.BatchNorm2d(64)
+    self.conv3 = nn.Conv2d(32, 64, 3, padding=1, bias=False)
+    self.bn3 = nn.BatchNorm2d(64)
 
-        self.conv4 = nn.Conv2d(64, 128, 3, padding=1, bias=False)
-        self.bn4 = nn.BatchNorm2d(128)
+    self.conv4 = nn.Conv2d(64, 128, 3, padding=1, bias=False)
+    self.bn4 = nn.BatchNorm2d(128)
 
-        self.conv5 = nn.Conv2d(128, 256, 3, padding=1, bias=False)
-        self.bn5 = nn.BatchNorm2d(256)
+    self.conv5 = nn.Conv2d(128, 256, 3, padding=1, bias=False)
+    self.bn5 = nn.BatchNorm2d(256)
 
-        self.fc1 = nn.Linear(256, 4)
+    self.conv6 = nn.Conv2d(256, 512, 3, padding=1, bias=False)
+    self.bn6 = nn.BatchNorm2d(512)
 
-    def forward(self, x):
-        # Input 64x64x3
+    self.conv7 = nn.Conv2d(512, 1024, 3, padding=1, bias=False)
+    self.bn7 = nn.BatchNorm2d(1024)
 
-        # x = self.conv1(x)
-        # x = self.bn1(x)
-        # x = F.relu(x)
-        # x = F.max_pool2d(x, kernel_size=2, stride=2)
+    self.fc4 = nn.Linear(1024, 4) # 4 categories
 
-        x = F.max_pool2d(F.relu(self.bn1(self.conv1(x))), kernel_size=2, stride=2) # 32x32x16
-        x = F.max_pool2d(F.relu(self.bn2(self.conv2(x))), kernel_size=2, stride=2) # 16x16x32
-        x = F.max_pool2d(F.relu(self.bn3(self.conv3(x))), kernel_size=2, stride=2) # 8x8x64
-        x = F.max_pool2d(F.relu(self.bn4(self.conv4(x))), kernel_size=2, stride=2) # 4x4x128
-        x = F.max_pool2d(F.relu(self.bn5(self.conv5(x))), kernel_size=2, stride=2) # 2x2x256
+  def forward(self, x):
+    # Input 256x256x3
+    x = F.max_pool2d(F.relu(self.bn1(self.conv1(x))), kernel_size=2, stride=2) # 128x128x16
+    x = F.max_pool2d(F.relu(self.bn2(self.conv2(x))), kernel_size=2, stride=2) # 64x64x32
+    x = F.max_pool2d(F.relu(self.bn3(self.conv3(x))), kernel_size=2, stride=2) # 32x32x64
+    x = F.max_pool2d(F.relu(self.bn4(self.conv4(x))), kernel_size=2, stride=2) # 16x16x128
+    x = F.max_pool2d(F.relu(self.bn5(self.conv5(x))), kernel_size=2, stride=2) # 8x8x256
+    x = F.max_pool2d(F.relu(self.bn6(self.conv6(x))), kernel_size=2, stride=2) # 4x4x512
+    x = F.max_pool2d(F.relu(self.bn7(self.conv7(x))), kernel_size=2, stride=2) # 2x2x1024
+    x = F.adaptive_avg_pool2d(x, 1) # 1x1x1024
+    x = torch.flatten(x, 1)
+    x = self.fc4(x)
+    return x
 
-        # Global average pooling across each channel (Input could be 2x2x256, 4x4x256, 7x3x256, output would always be 256 length vector)
-        x = F.adaptive_avg_pool2d(x, 1)                                            # 1x1x256
-        x = torch.flatten(x, 1)                                                    # vector 256
-        
-        x = self.fc1(x)
-        return x
-
+# https://colab.research.google.com/drive/1EBz4feoaUvz-o_yeMI27LEQBkvrXNc_4?usp=sharing#scrollTo=9tH2B0mlQywk
 def train(net, dataloader, epochs=1, start_epoch=0, lr=0.01, momentum=0.9, decay=0.0005, 
           verbose=1, print_every=10, state=None, schedule={}, checkpoint_path=None):
   net.to(device)
@@ -77,10 +76,10 @@ def train(net, dataloader, epochs=1, start_epoch=0, lr=0.01, momentum=0.9, decay
 
   # Load previous training state
   if state:
-      net.load_state_dict(state['net'])
-      optimizer.load_state_dict(state['optimizer'])
-      start_epoch = state['epoch']
-      losses = state['losses']
+    net.load_state_dict(state['net'])
+    optimizer.load_state_dict(state['optimizer'])
+    start_epoch = state['epoch']
+    losses = state['losses']
 
   # Fast forward lr schedule through already trained epochs
   for epoch in range(start_epoch):
@@ -99,22 +98,22 @@ def train(net, dataloader, epochs=1, start_epoch=0, lr=0.01, momentum=0.9, decay
         g['lr'] = schedule[epoch]
 
     for i, batch in enumerate(dataloader, 0):
-        inputs, labels = batch[0].to(device), batch[1].to(device)
+      inputs, labels = batch[0].to(device), batch[1].to(device)
 
-        optimizer.zero_grad()
+      optimizer.zero_grad()
 
-        outputs = net(inputs)
-        loss = criterion(outputs, labels)
-        loss.backward()  # autograd magic, computes all the partial derivatives
-        optimizer.step() # takes a step in gradient direction
+      outputs = net(inputs)
+      loss = criterion(outputs, labels)
+      loss.backward()  # autograd magic, computes all the partial derivatives
+      optimizer.step() # takes a step in gradient direction
 
-        losses.append(loss.item())
-        sum_loss += loss.item()
+      losses.append(loss.item())
+      sum_loss += loss.item()
         
-        if i % print_every == print_every-1:    # print every 10 mini-batches
-            if verbose:
-              print('[%d, %5d] loss: %.3f' % (epoch, i + 1, sum_loss / print_every))
-            sum_loss = 0.0
+      if i % print_every == print_every-1:
+        if verbose:
+          print('[%d, %5d] loss: %.3f' % (epoch, i + 1, sum_loss / print_every))
+        sum_loss = 0.0
     if checkpoint_path:
       state = {'epoch': epoch+1, 'net': net.state_dict(), 'optimizer': optimizer.state_dict(), 'losses': losses}
       torch.save(state, checkpoint_path + 'checkpoint-%d.pkl'%(epoch+1))
@@ -126,20 +125,22 @@ def accuracy(net, dataloader):
   correct = 0
   total = 0
   with torch.no_grad():
-      for batch in dataloader:
-          images, labels = batch[0].to(device), batch[1].to(device)
-          outputs = net(images)
-          _, predicted = torch.max(outputs.data, 1)
-          total += labels.size(0)
-          correct += (predicted == labels).sum().item()
+    for batch in dataloader:
+      images, labels = batch[0].to(device), batch[1].to(device)
+      outputs = net(images)
+      _, predicted = torch.max(outputs.data, 1)
+      total += labels.size(0)
+      correct += (predicted == labels).sum().item()
   return correct/total
 
 def smooth(x, size):
   return np.convolve(x, np.ones(size)/size, mode='valid')
 
 if __name__ == '__main__':
-  net = Darknet64()
-  losses = train(net, trainloader, epochs=20, schedule={0:.01, 5:.001, 10:.0001})
+  net = DarknetUpdated()
+  losses = train(net, trainloader, epochs=25, schedule={0:0.01, 5:0.001, 15:0.0001})
   print("Testing accuracy: %f" % accuracy(net, testloader))
-  plt.plot(smooth(losses,50))
-  plt.show()
+
+  # Used to display the loss curve
+  # plt.plot(smooth(losses,50))
+  # plt.show()
